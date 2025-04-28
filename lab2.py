c71059477903875ec18f4fcc82c46c4876de75a1
Terence <terencetng.24@ichat.sp.edu.sg>
2025-04-28 15:09:31 +0800

    added bmi scale

--- lab2.py
+++ lab2.py
@@ -1,1 +1,17 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height*height)
+    print(f"BMI = {bmi:.2f}")
+    if bmi < 18.5:
+        print("Under Weight")
+    elif bmi > 25:
+        print("Over Weight")
+    else:
+        print("Normal Weight")
+
+
+calculate_bmi(weight=57, height=1.73)
